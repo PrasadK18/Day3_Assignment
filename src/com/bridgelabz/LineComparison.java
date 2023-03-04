5c24cd2e8af97520d13e6cb81e50461286bbffb5
PRASAD KADAM <prasadkadam7743@gmail.com>
2023-03-04 20:18:57 +0530

    UC1_CalculateLength:-Added

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -1,9 +1,29 @@
 package com.bridgelabz;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to Line\n" +
                 "Comparison Computation\n" +
                 "Program on Master Branch");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter x1: ");
+        double x1 = sc.nextDouble();
+        System.out.print("Enter y1: ");
+        double y1 = sc.nextDouble();
+        System.out.print("Enter x2: ");
+        double x2 = sc.nextDouble();
+        System.out.print("Enter y2: ");
+        double y2 = sc.nextDouble();
+
+        double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+        System.out.println("The length of the line segment is: " + length);
+
+
     }
 }
